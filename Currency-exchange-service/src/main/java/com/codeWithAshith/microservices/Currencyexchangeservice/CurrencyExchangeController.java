afe8ad98960df119c43c5a8a1659adee7b4cc0b7
Ashith V L <ashithvl@gmail.com>
2022-08-01 00:50:39 +0530

    9._JPA_setup_complete

--- Currency-exchange-service/src/main/java/com/codeWithAshith/microservices/Currencyexchangeservice/CurrencyExchangeController.java
+++ Currency-exchange-service/src/main/java/com/codeWithAshith/microservices/Currencyexchangeservice/CurrencyExchangeController.java
@@ -10,20 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
-	
+
 	@Autowired
 	private Environment evironment;
 
+	@Autowired
+	private ExchangeValueRepository exchangeValueRepository;
+
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(evironment.getProperty("local.server.port")));
 		return exchangeValue;
 	}
-	
-	// Right click on project and choose run as -> run configuration 
+
+	// Right click on project and choose run as -> run configuration
 	// Rename the project by adding a suffix of 8000
 	// Duplicate the configuration and rename with a suffix 8001
 	// Switch to arguments tab and enter "-Dserver.port=8001" in VM arguments
-	
+
 }
