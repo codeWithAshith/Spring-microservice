01379befb37c8ade33040f5f2715bf240ab8a3d8
Ashith V L <ashithvl@gmail.com>
2022-07-31 18:35:08 +0530

    3._Connect_Spring_Cloud_Config_Server_to_local_git_repository

--- spring-cloud-config-server/src/main/java/com/codeWithAshith/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/codeWithAshith/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,12 +2,21 @@ package com.codeWithAshith.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+// add enable config server
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
+	
+	// create a folder called - git-localconfig 
+	// link it to the spring-cloud-config-server project by
+	// Right click on project name -> Build path -> Link source -> choose git-localconfig 
+	// In git-localconfig create properties file limits-service.properties 
+	// Add, commit to local git
 
 }
